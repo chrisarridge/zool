d6728aae8989d909d9b23fe3ee2941db0d555e58
Chris Arridge <48034138+chrisarridge@users.noreply.github.com>
2023-06-10 23:40:24 +0100

    Moved exceptions into separate module

---
+++ source/zool/exceptions.py
@@ -0,0 +1,87 @@
+"""Exceptions class definitions.
+"""
+
+class ZoolError(Exception):
+    """Base class for Zool exceptions."""
+    pass
+
+class InappropriateConstraint(ZoolError):
+    """Exception raised for errors in the constraints.
+
+    Attributes
+    ----------
+    constraint : str
+        The constraint.
+    id : str
+        The id that the constraint was given.
+    message : str
+        The message accompanying the error.
+    """
+
+    def __init__(self, constraint: str, id: str, message: str):
+        """Initialise.
+
+        Parameters
+        ----------
+        constraint : str
+            The constraint.
+        id : str
+            The id that the constraint was given.
+        message : str
+            The message accompanying the error.
+        """
+        self.constraint = constraint
+        self.id = id
+        self.message = message
+
+
+class NoSolution(ZoolError):
+    """Exception raised where the user has tried to generate axes that have not been solved.
+    """
+
+
+class UnknownElement(ZoolError):
+    """Exception raised for where an unknown element id is encountered.
+
+    Attributes
+    ----------
+    id : str
+        The id that was unknown.
+    """
+
+    def __init__(self, id: str):
+        """Initialise.
+
+        Parameters
+        ----------
+
+        id : str
+            The id that was unknown.
+        """
+        self.id = id
+
+
+
+class DeserialisationError(ZoolError):
+    """Constraint cannot be deserialised because the dictionary isn't complete.
+
+    Attributes
+    ----------
+    constraint : str
+        The constraint.
+    message : str
+        The message accompanying the error.
+    """
+
+    def __init__(self, constraint: str, message: str):
+        """Initialise.
+
+        Parameters
+        ----------
+        constraint : str
+            The constraint.
+        message : str
+            The message accompanying the error.
+        """
+        self.constraint = constraint
+        self.message = message
