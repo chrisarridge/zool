a4aee8f74fcaf405f19e4f9c9a91708b21478a75
Chris Arridge <48034138+chrisarridge@users.noreply.github.com>
2023-11-27 09:11:28 +0000

    Reformatted using isort/black/flake8 and added __all__ for public interface.

--- source/zool/__init__.py
+++ source/zool/__init__.py
@@ -1,8 +1,4 @@
-## ==========================================================================
-##
-## Module metadata.
-##
-## ==========================================================================
+# Module metadata.
 __author__ = "Chris Arridge/Lancaster University"
 __copyright__ = "Copyright 2017-2023 Lancaster University"
 __version__ = "2.1"
@@ -11,6 +7,28 @@ __status__ = "beta"
 __date__ = "11 January 2023"
 __docformat__ = "numpy"
 
-from .core import *
-from .factory import *
-from .constraints import Fixed, FixedAspect, FromChildren, FromParent, Fill, Named
+from .constraints import (
+    Fill,
+    Fixed,
+    FixedAspect,
+    FromChildren,
+    FromParent,
+    Named,
+)
+from .core import PlotElement, Layout
+from .factory import vertical_stack, triangle, triangle_equal, subplot
+
+__all__ = [
+    "Fill",
+    "Fixed",
+    "FixedAspect",
+    "FromChildren",
+    "FromParent",
+    "Named",
+    "PlotElement",
+    "Layout",
+    "vertical_stack",
+    "triangle",
+    "triangle_equal",
+    "subplot",
+]
