14c6eca922f7bd8b634998ba67e103b60fb7f385
Chris Arridge <48034138+chrisarridge@users.noreply.github.com>
2023-11-27 09:11:09 +0000

    Reformatted using isort/black/flake8.

--- source/zool/constraints.py
+++ source/zool/constraints.py
@@ -1,19 +1,21 @@
 """Constraints and constraint serialisers/deserialisers
 """
-from typing import Union
 from dataclasses import dataclass
+from typing import Union
 
 from .exceptions import DeserialisationError
 
+
 @dataclass
 class Fixed:
-    """Class for storing a fixed width or fixed height size constraint.
+    """Constraint for a fixed width or height size.
 
     Attributes
     ----------
     size : float
         The width or height for this fixed constraint.
     """
+
     size: float
 
     def to_dict(self) -> dict:
@@ -23,19 +25,19 @@ class Fixed:
         -------
         dict
         """
-        return {"constraint":"fixed","size":self.size}
-
+        return {"constraint": "fixed", "size": self.size}
 
 
 @dataclass
 class FixedAspect:
-    """Class for storing a fixed aspect ratio constraint.
+    """Constraint with a fixed aspect ratio.
 
     Attributes
     ----------
     aspect : float
         Aspect ratio (width/height) for this fixed constraint.
     """
+
     aspect: float
 
     def to_dict(self) -> dict:
@@ -45,13 +47,11 @@ class FixedAspect:
         -------
         dict
         """
-        return {"constraint":"fixedAspect","aspect":self.aspect}
-
+        return {"constraint": "fixedAspect", "aspect": self.aspect}
 
 
 class FromChildren:
-    """Class to store a constraint where the width or height comes from any child elements.
-    """
+    """Constraint where the width or height comes from any child elements."""
 
     def to_dict(self) -> dict:
         """Convert this constraint into a dictionary
@@ -60,12 +60,11 @@ class FromChildren:
         -------
         dict
         """
-        return {"constraint":"fromChildren"}
+        return {"constraint": "fromChildren"}
 
 
 class FromParent:
-    """Class to store a constraint where the width or height comes from the parent element.
-    """
+    """Constraint where a width or height comes from the parent element."""
 
     def to_dict(self) -> dict:
         """Convert this constraint into a dictionary
@@ -74,12 +73,11 @@ class FromParent:
         -------
         dict
         """
-        return {"constraint":"fromParent"}
+        return {"constraint": "fromParent"}
 
 
 class Fill:
-    """Class to store a constraint where the width or height fills any remaining space in the parent element.
-    """
+    """Constraint where width or height set to fill parent element."""
 
     def to_dict(self) -> dict:
         """Convert this constraint into a dictionary
@@ -88,19 +86,19 @@ class Fill:
         -------
         dict
         """
-        return {"constraint":"fill"}
-
+        return {"constraint": "fill"}
 
 
 @dataclass
 class Named:
-    """Class to store a constraint where a width or height is taken from another element.
-    
+    """Constraint where width or height comes from another element.
+
     Attributes
     ----------
     id : str
         Id string to point to the element.
     """
+
     id: str
 
     def to_dict(self) -> dict:
@@ -110,11 +108,12 @@ class Named:
         -------
         dict
         """
-        return {"constraint":"named", "id":self.id}
+        return {"constraint": "named", "id": self.id}
 
 
-
-def constraint_deserialiser(v: dict) -> Union[Fixed,FixedAspect,FromChildren,FromParent,Fill,Named]:
+def constraint_deserialiser(
+    v: dict,
+) -> Union[Fixed, FixedAspect, FromChildren, FromParent, Fill, Named]:
     """Factory function to turn serialised constraints back into constraints
 
     Parameters
@@ -132,38 +131,49 @@ def constraint_deserialiser(v: dict) -> Union[Fixed,FixedAspect,FromChildren,Fro
     TypeError
         If the supplied serialised constraint isn't a dictionary.
     DeserialisationError
-        If the serialised constraint is missing information or contains a constraint we don't know about.
+        If the serialised constraint is incomplete or unknown.
     """
 
     if not isinstance(v, dict):
         raise TypeError("Supplied variable is not a dictionary")
     if "constraint" not in v:
-        raise DeserialisationError("Unknown","Supplied dictionary does not have a <constraint> entry")
+        raise DeserialisationError(
+            "Unknown", "Supplied dictionary does not have a <constraint> entry"
+        )
 
-    if v["constraint"]=="fixed":
+    if v["constraint"] == "fixed":
         if "size" not in v:
-            raise DeserialisationError("Fixed","Require a value for a fixed constraint")
+            raise DeserialisationError(
+                "Fixed", "Require a value for a fixed constraint"
+            )
         return Fixed(float(v["size"]))
 
-    elif v["constraint"]=="fixedAspect":
+    elif v["constraint"] == "fixedAspect":
         if "aspect" not in v:
-            raise DeserialisationError("FixedAspect","Require a value for a fixedAspect constraint")
+            raise DeserialisationError(
+                "FixedAspect", "Require a value for a fixedAspect constraint"
+            )
         return FixedAspect(float(v["aspect"]))
 
-    elif v["constraint"]=="named":
+    elif v["constraint"] == "named":
         if "id" not in v:
-            raise DeserialisationError("Named","Require a panel id for a named width/height constraint")
+            raise DeserialisationError(
+                "Named",
+                "Require a panel id for a named width/height constraint",
+            )
         return Named(v["id"])
 
-    elif v["constraint"]=="fill":
+    elif v["constraint"] == "fill":
         return Fill()
 
-    elif v["constraint"]=="fromChildren":
+    elif v["constraint"] == "fromChildren":
         return FromChildren()
 
-    elif v["constraint"]=="fromParent":
+    elif v["constraint"] == "fromParent":
         return FromParent()
 
     else:
-        raise DeserialisationError(v["constraint"], "Unknown constraint type <{}>".format(v["constraint"]))
-
+        raise DeserialisationError(
+            v["constraint"],
+            "Unknown constraint type <{}>".format(v["constraint"]),
+        )
