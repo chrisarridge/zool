c47893c3817398b70011d980b0ae616fd73e7eca
Chris Arridge <48034138+chrisarridge@users.noreply.github.com>
2023-11-27 09:08:47 +0000

    Reformatted using isort/black and checked with flake8

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,32 @@
 """Testing of various layouts."""
 
-import zool
 import unittest
+
+import zool
 
 
 class TestFactory(unittest.TestCase):
-	def test(self):
-		tmp = zool.vertical_stack([5,5,10,10], padding=2, margin_left=2,
-				margin_right=2.0, margin_top=2.0, margin_bottom=2,
-				w=20)
-		self.assertEqual(tmp.figure_width, 24)
-		self.assertEqual(tmp.figure_height, 40)
-		self.assertEqual(tmp['3'].yb, 2)
-		self.assertEqual(tmp['3'].yt, 2+10)
-		self.assertEqual(tmp['2'].yb, 2+10+2)
-		self.assertEqual(tmp['2'].yt, 2+10+2+10)
-		self.assertEqual(tmp['1'].yb, 2+10+2+10+2)
-		self.assertEqual(tmp['1'].yt, 2+10+2+10+2+5)
-		self.assertEqual(tmp['0'].yb, 2+10+2+10+2+5+2)
-		self.assertEqual(tmp['0'].yt, 2+10+2+10+2+5+2+5)
+    def test(self):
+        tmp = zool.vertical_stack(
+            [5, 5, 10, 10],
+            padding=2,
+            margin_left=2,
+            margin_right=2.0,
+            margin_top=2.0,
+            margin_bottom=2,
+            w=20,
+        )
+        self.assertEqual(tmp.figure_width, 24)
+        self.assertEqual(tmp.figure_height, 40)
+        self.assertEqual(tmp["3"].yb, 2)
+        self.assertEqual(tmp["3"].yt, 2 + 10)
+        self.assertEqual(tmp["2"].yb, 2 + 10 + 2)
+        self.assertEqual(tmp["2"].yt, 2 + 10 + 2 + 10)
+        self.assertEqual(tmp["1"].yb, 2 + 10 + 2 + 10 + 2)
+        self.assertEqual(tmp["1"].yt, 2 + 10 + 2 + 10 + 2 + 5)
+        self.assertEqual(tmp["0"].yb, 2 + 10 + 2 + 10 + 2 + 5 + 2)
+        self.assertEqual(tmp["0"].yt, 2 + 10 + 2 + 10 + 2 + 5 + 2 + 5)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
